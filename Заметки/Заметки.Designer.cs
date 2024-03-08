6b0a49860140d2def01af8f8ceef2c06aa29428e
user <user@DESKTOP-IJ7D573>
2024-03-08 16:12:56 +0400

    Добавьте файлы проекта.

---
+++ Заметки/Заметки.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace Заметки
+{
+    partial class Заметки
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Заметки));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Заголовок = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Содержание = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.guna2ContextMenuStrip1 = new Guna.UI2.WinForms.Guna2ContextMenuStrip();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.TextBoxSearch = new Guna.UI2.WinForms.Guna2TextBox();
+            this.guna2BorderlessForm1 = new Guna.UI2.WinForms.Guna2BorderlessForm(this.components);
+            this.guna2ControlBox1 = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.guna2ControlBox2 = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.TextBoxTitle = new Guna.UI2.WinForms.Guna2TextBox();
+            this.TextBoxContent = new Guna.UI2.WinForms.Guna2TextBox();
+            this.Add = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2ContextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(24)))), ((int)(((byte)(24)))));
+            this.listView1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Заголовок,
+            this.Содержание});
+            this.listView1.ContextMenuStrip = this.guna2ContextMenuStrip1;
+            this.listView1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.listView1.ForeColor = System.Drawing.Color.White;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(3, 3);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(461, 516);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // Заголовок
+            // 
+            this.Заголовок.Text = "Заголовок";
+            this.Заголовок.Width = 135;
+            // 
+            // Содержание
+            // 
+            this.Содержание.Text = "Содержание";
+            this.Содержание.Width = 154;
+            // 
+            // guna2ContextMenuStrip1
+            // 
+            this.guna2ContextMenuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(24)))), ((int)(((byte)(24)))));
+            this.guna2ContextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.deleteToolStripMenuItem});
+            this.guna2ContextMenuStrip1.Name = "guna2ContextMenuStrip1";
+            this.guna2ContextMenuStrip1.RenderStyle.ArrowColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(143)))), ((int)(((byte)(255)))));
+            this.guna2ContextMenuStrip1.RenderStyle.BorderColor = System.Drawing.Color.Empty;
+            this.guna2ContextMenuStrip1.RenderStyle.ColorTable = null;
+            this.guna2ContextMenuStrip1.RenderStyle.RoundedEdges = true;
+            this.guna2ContextMenuStrip1.RenderStyle.SelectionArrowColor = System.Drawing.Color.White;
+            this.guna2ContextMenuStrip1.RenderStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.guna2ContextMenuStrip1.RenderStyle.SelectionForeColor = System.Drawing.Color.White;
+            this.guna2ContextMenuStrip1.RenderStyle.SeparatorColor = System.Drawing.Color.Gainsboro;
+            this.guna2ContextMenuStrip1.RenderStyle.TextRenderingHint = System.Drawing.Text.TextRenderingHint.SystemDefault;
+            this.guna2ContextMenuStrip1.Size = new System.Drawing.Size(165, 26);
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.deleteToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("deleteToolStripMenuItem.Image")));
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(164, 22);
+            this.deleteToolStripMenuItem.Text = "Удалить заметку";
+            this.deleteToolStripMenuItem.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click);
+            // 
+            // TextBoxSearch
+            // 
+            this.TextBoxSearch.Animated = true;
+            this.TextBoxSearch.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.TextBoxSearch.BorderRadius = 10;
+            this.TextBoxSearch.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.TextBoxSearch.DefaultText = "";
+            this.TextBoxSearch.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.TextBoxSearch.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.TextBoxSearch.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.TextBoxSearch.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.TextBoxSearch.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(24)))), ((int)(((byte)(24)))));
+            this.TextBoxSearch.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.TextBoxSearch.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.TextBoxSearch.ForeColor = System.Drawing.Color.White;
+            this.TextBoxSearch.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.TextBoxSearch.IconLeft = ((System.Drawing.Image)(resources.GetObject("TextBoxSearch.IconLeft")));
+            this.TextBoxSearch.Location = new System.Drawing.Point(470, 5);
+            this.TextBoxSearch.Name = "TextBoxSearch";
+            this.TextBoxSearch.PasswordChar = '\0';
+            this.TextBoxSearch.PlaceholderForeColor = System.Drawing.Color.White;
+            this.TextBoxSearch.PlaceholderText = "Поиск по ключевому слову";
+            this.TextBoxSearch.SelectedText = "";
+            this.TextBoxSearch.Size = new System.Drawing.Size(230, 27);
+            this.TextBoxSearch.TabIndex = 1;
+            this.TextBoxSearch.TextChanged += new System.EventHandler(this.TextBoxSearch_TextChanged);
+            // 
+            // guna2BorderlessForm1
+            // 
+            this.guna2BorderlessForm1.BorderRadius = 16;
+            this.guna2BorderlessForm1.ContainerControl = this;
+            this.guna2BorderlessForm1.DockIndicatorTransparencyValue = 0.6D;
+            this.guna2BorderlessForm1.DragStartTransparencyValue = 1D;
+            this.guna2BorderlessForm1.HasFormShadow = false;
+            this.guna2BorderlessForm1.ResizeForm = false;
+            this.guna2BorderlessForm1.TransparentWhileDrag = true;
+            // 
+            // guna2ControlBox1
+            // 
+            this.guna2ControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2ControlBox1.BorderRadius = 10;
+            this.guna2ControlBox1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(24)))), ((int)(((byte)(24)))));
+            this.guna2ControlBox1.HoverState.FillColor = System.Drawing.Color.Red;
+            this.guna2ControlBox1.IconColor = System.Drawing.Color.White;
+            this.guna2ControlBox1.Location = new System.Drawing.Point(757, 3);
+            this.guna2ControlBox1.Name = "guna2ControlBox1";
+            this.guna2ControlBox1.Size = new System.Drawing.Size(45, 29);
+            this.guna2ControlBox1.TabIndex = 2;
+            // 
+            // guna2ControlBox2
+            // 
+            this.guna2ControlBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2ControlBox2.BorderRadius = 10;
+            this.guna2ControlBox2.ControlBoxType = Guna.UI2.WinForms.Enums.ControlBoxType.MinimizeBox;
+            this.guna2ControlBox2.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(24)))), ((int)(((byte)(24)))));
+            this.guna2ControlBox2.IconColor = System.Drawing.Color.White;
+            this.guna2ControlBox2.Location = new System.Drawing.Point(706, 3);
+            this.guna2ControlBox2.Name = "guna2ControlBox2";
+            this.guna2ControlBox2.Size = new System.Drawing.Size(45, 29);
+            this.guna2ControlBox2.TabIndex = 3;
+            // 
+            // TextBoxTitle
+            // 
+            this.TextBoxTitle.Animated = true;
+            this.TextBoxTitle.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.TextBoxTitle.BorderRadius = 10;
+            this.TextBoxTitle.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.TextBoxTitle.DefaultText = "";
+            this.TextBoxTitle.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.TextBoxTitle.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.TextBoxTitle.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.TextBoxTitle.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.TextBoxTitle.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(24)))), ((int)(((byte)(24)))));
+            this.TextBoxTitle.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.TextBoxTitle.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.TextBoxTitle.ForeColor = System.Drawing.Color.White;
+            this.TextBoxTitle.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.TextBoxTitle.IconLeft = ((System.Drawing.Image)(resources.GetObject("TextBoxTitle.IconLeft")));
+            this.TextBoxTitle.Location = new System.Drawing.Point(513, 173);
+            this.TextBoxTitle.Name = "TextBoxTitle";
+            this.TextBoxTitle.PasswordChar = '\0';
+            this.TextBoxTitle.PlaceholderForeColor = System.Drawing.Color.White;
+            this.TextBoxTitle.PlaceholderText = "Заголовок";
+            this.TextBoxTitle.SelectedText = "";
+            this.TextBoxTitle.Size = new System.Drawing.Size(230, 29);
+            this.TextBoxTitle.TabIndex = 4;
+            // 
+            // TextBoxContent
+            // 
+            this.TextBoxContent.Animated = true;
+            this.TextBoxContent.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.TextBoxContent.BorderRadius = 10;
+            this.TextBoxContent.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.TextBoxContent.DefaultText = "";
+            this.TextBoxContent.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.TextBoxContent.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.TextBoxContent.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.TextBoxContent.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.TextBoxContent.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(24)))), ((int)(((byte)(24)))));
+            this.TextBoxContent.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.TextBoxContent.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.TextBoxContent.ForeColor = System.Drawing.Color.White;
+            this.TextBoxContent.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.TextBoxContent.IconLeft = ((System.Drawing.Image)(resources.GetObject("TextBoxContent.IconLeft")));
+            this.TextBoxContent.Location = new System.Drawing.Point(513, 221);
+            this.TextBoxContent.Name = "TextBoxContent";
+            this.TextBoxContent.PasswordChar = '\0';
+            this.TextBoxContent.PlaceholderForeColor = System.Drawing.Color.White;
+            this.TextBoxContent.PlaceholderText = "Содержание";
+            this.TextBoxContent.SelectedText = "";
+            this.TextBoxContent.Size = new System.Drawing.Size(230, 29);
+            this.TextBoxContent.TabIndex = 5;
+            // 
+            // Add
+            // 
+            this.Add.Animated = true;
+            this.Add.BorderRadius = 10;
+            this.Add.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.Add.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.Add.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.Add.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.Add.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.Add.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.Add.ForeColor = System.Drawing.Color.White;
+            this.Add.Location = new System.Drawing.Point(513, 265);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(230, 34);
+            this.Add.TabIndex = 6;
+            this.Add.Text = "Добавить заметку";
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // Заметки
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(24)))), ((int)(((byte)(24)))));
+            this.ClientSize = new System.Drawing.Size(804, 521);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.TextBoxContent);
+            this.Controls.Add(this.TextBoxTitle);
+            this.Controls.Add(this.guna2ControlBox2);
+            this.Controls.Add(this.guna2ControlBox1);
+            this.Controls.Add(this.TextBoxSearch);
+            this.Controls.Add(this.listView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Заметки";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Заметки";
+            this.guna2ContextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Заголовок;
+        private System.Windows.Forms.ColumnHeader Содержание;
+        private Guna.UI2.WinForms.Guna2TextBox TextBoxSearch;
+        private Guna.UI2.WinForms.Guna2BorderlessForm guna2BorderlessForm1;
+        private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox2;
+        private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox1;
+        private Guna.UI2.WinForms.Guna2TextBox TextBoxTitle;
+        private Guna.UI2.WinForms.Guna2TextBox TextBoxContent;
+        private Guna.UI2.WinForms.Guna2Button Add;
+        private Guna.UI2.WinForms.Guna2ContextMenuStrip guna2ContextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+    }
+}
+
---
+++ Заметки/Заметки.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace Заметки
+{
+    /*
+   ______          __         __   __             _  __ __  _______ _       __      __ __
+  / ____/___  ____/ /__  ____/ /  / /_  __  __   | |/ //  |/  ( __ ) |     / /_  __/ //_/
+ / /   / __ \/ __  / _ \/ __  /  / __ \/ / / /   |   // /|_/ / __  | | /| / / / / / ,<   
+/ /___/ /_/ / /_/ /  __/ /_/ /  / /_/ / /_/ /   /   |/ /  / / /_/ /| |/ |/ / /_/ / /| |  
+\____/\____/\__,_/\___/\__,_/  /_.___/\__, /   /_/|_/_/  /_/\____/ |__/|__/\__, /_/ |_|  
+                                     /____/                               /____/         
+     */
+    public partial class Заметки : Form
+    {
+        private string connectionString = "Data Source=notes.db;Version=3;"; // Конектимся к базе данных SQLite
+        public Заметки()
+        {
+            InitializeComponent();
+            CreateTable(); // Создаем таблицу при запуске формы           
+            UpdateNoteList(); // Обновляем список заметок при запуске формы
+            TextBoxSearch.TextChanged += TextBoxSearch_TextChanged;
+        }
+        // При нажатие кнопки добавляет определеную заметку
+        private void Add_Click(object sender, EventArgs e)
+        {
+            string title = TextBoxTitle.Text.Trim();
+            string noteText = TextBoxContent.Text.Trim();
+            if (!string.IsNullOrEmpty(title) && !string.IsNullOrEmpty(noteText))
+            {
+                CreateNote(title, noteText);
+                UpdateNoteList();
+                TextBoxTitle.Clear();
+                TextBoxContent.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Введите заголовок и текст заметки.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        // Поиск заметок по ключевому слову из TextBox
+        private void TextBoxSearch_TextChanged(object sender, EventArgs e)
+        {
+            string keyword = TextBoxSearch.Text.Trim();
+            if (!string.IsNullOrEmpty(keyword))
+            {
+                SearchNotes(keyword);
+            }
+            else
+            {
+                UpdateNoteList();
+            }
+        }
+        // Создания таблицы в базе данных
+        private void CreateTable()
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                string sql = "CREATE TABLE IF NOT EXISTS Notes (Id INTEGER PRIMARY KEY AUTOINCREMENT, Title TEXT, Text TEXT)";
+                using (SQLiteCommand command = new SQLiteCommand(sql, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+        // Метод Поиска заметок по ключевому слову
+        private void SearchNotes(string keyword)
+        {
+            listView1.Items.Clear();
+
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                string sql = "SELECT * FROM Notes WHERE Title COLLATE UTF8_GENERAL_CI LIKE @Keyword OR Text COLLATE UTF8_GENERAL_CI LIKE @Keyword";
+                using (SQLiteCommand command = new SQLiteCommand(sql, connection))
+                {
+                    command.Parameters.AddWithValue("@Keyword", "%" + keyword + "%");
+
+                    using (SQLiteDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            ListViewItem item = new ListViewItem(reader["Title"].ToString());
+                            item.SubItems.Add(reader["Text"].ToString());
+                            listView1.Items.Add(item);
+                        }
+                    }
+                }
+            }
+        }
+        // Обновления списка заметок на форме
+        private void UpdateNoteList()
+        {
+            listView1.Items.Clear();
+
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                string sql = "SELECT * FROM Notes";
+                using (SQLiteCommand command = new SQLiteCommand(sql, connection))
+                {
+                    using (SQLiteDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            ListViewItem item = new ListViewItem(reader["Title"].ToString());
+                            item.SubItems.Add(reader["Text"].ToString());
+                            listView1.Items.Add(item);
+                        }
+                    }
+                }
+            }
+        }
+        // Метод создания новой заметки в базе данных
+        private void CreateNote(string title, string text)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                string sql = "INSERT INTO Notes (Title, Text) VALUES (@Title, @Text)";
+                using (SQLiteCommand command = new SQLiteCommand(sql, connection))
+                {
+                    command.Parameters.AddWithValue("@Title", title);
+                    command.Parameters.AddWithValue("@Text", text);
+                    command.ExecuteNonQuery();
+                }
+            }
+
+            MessageBox.Show("Заметка успешно создана.", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        // Метод удаления заметки из базы данных
+        private void DeleteNoteByTitle(string title)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                string sql = "DELETE FROM Notes WHERE Title = @Title";
+                using (SQLiteCommand command = new SQLiteCommand(sql, connection))
+                {
+                    command.Parameters.AddWithValue("@Title", title);
+                    int rowsAffected = command.ExecuteNonQuery();
+                    if (rowsAffected == 0)
+                    {
+                        MessageBox.Show("Заметка с указанным заголовком не найдена.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Заметка успешно удалена.", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+        }
+        // Удаляем при нажатие на кнопку заметку из базы данных
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                string noteTitleToDelete = listView1.SelectedItems[0].Text;
+                DeleteNoteByTitle(noteTitleToDelete);
+                UpdateNoteList();
+            }
+            else
+            {
+                MessageBox.Show("Выберите заметку для удаления.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
